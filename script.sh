8e4e73b1ea3eab98fb973e4dd4eba5488ff196e8
Vladislav Akulov <vladiislav.akulov@gmail.com>
2021-08-19 18:29:24 +0300

    Script for adding users

---
+++ script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+while :
+do 
+#Get username, user password and home directory
+IFS=" "
+echo "Please type user name,user password and home directory"
+read user_name user_password home_directory
+echo "Please type user groups"
+read -a groups
+#Creating groups
+for group in $groups
+do
+sudo groupadd $group
+done
+#Creating hash
+hash=$(openssl passwd $user_password)
+#Add user
+sudo useradd -g "$groups" --home-dir "/home/$home_directory" --password "$hash" $user_name
+#Check for adding a any users
+echo "Do you want add any users?(Y for continue, any symbol for exit)"
+read answer
+if [ "$answer" == "Y" ]
+then
+continue
+else
+break
+fi
+done
+
